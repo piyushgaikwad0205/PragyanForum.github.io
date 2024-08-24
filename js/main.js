83b9e058aab35145eeedf11ea6877dbbd8baab77
piyushgaikwad0205 <gaikwadpiysh488@gmail.com>
2024-08-24 11:56:53 +0530

    add testimoninal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
- AOS.init({
+$(document).ready(function () {
+    var silder = $(".owl-carousel");
+    silder.owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: false,
+        items: 1,
+        stagePadding: 20,
+        center: true,
+        nav: false,
+        margin: 50,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: { items: 1 },
+            480: { items: 2 },
+            575: { items: 2 },
+            768: { items: 2 },
+            991: { items: 3 },
+            1200: { items: 4 }
+        }
+    });
+});
+AOS.init({
  	duration: 800,
  	easing: 'slide'
  });
